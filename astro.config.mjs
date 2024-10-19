6420585e25d0b84706b71ddaba0917e8f4da5d66
Xavier Ong <xavier.oyj1999@gmail.com>
2024-10-19 15:19:22 +0800

    Remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xavieroyj.dev',
-  trailingSlash: 'ignore',
+  trailingSlash: 'never',
   integrations: [tailwind(), react()]
 });
