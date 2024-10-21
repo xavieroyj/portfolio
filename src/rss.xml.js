5db804a88309b1aa6d7d4ddebbae9260a9565882
Xavier Ong <xavier.oyj1999@gmail.com>
2024-10-21 17:55:34 +0800

    Added RSS Feed

---
+++ src/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+    const blog = await getCollection("blogs", ({ data }) => {
+        return data.draft !== true;
+    });
+
+    return rss({
+        title: 'Xavier’s Blog',
+        description: 'Ramblings about what i’ve learnt so far. Mostly technologies, sometimes lifestyle.',
+        site: context.site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            // Compute RSS link from post `slug`
+            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            link: `/blogs/${post.slug}/`,
+        })),
+    });
+}
