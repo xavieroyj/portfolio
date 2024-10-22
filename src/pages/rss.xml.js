df18ab00a103f53b0b4468da71e306be85109f43
Xavier Ong <xavier.oyj1999@gmail.com>
2024-10-22 10:12:06 +0800

    Added content to the RSS feed. Sanitize body of the RSS feed to ensure no malicious input

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
     const blog = await getCollection("blogs", ({ data }) => {
@@ -17,6 +20,9 @@ export async function GET(context) {
             // Compute RSS link from post `slug`
             // This example assumes all posts are rendered as `/blog/[slug]` routes
             link: `/blogs/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
         })),
     });
 }
