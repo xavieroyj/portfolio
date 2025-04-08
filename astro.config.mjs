913276e80a32908bff50f9d0ff5a9107604b7f51
Xavier Ong <xavier.oyj1999@gmail.com>
2025-04-08 15:11:55 +0800

    Updated resume and updated dependencies. - Heavily rely more on Lucide Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,27 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { remarkReadingTime } from './integrations/remark-reading-time.mjs';
 import icon from "astro-icon";
-
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xavieroyj.dev',
+
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  trailingSlash: 'never',
+
   integrations: [
-    tailwind(),
     react(),
     sitemap(),
     icon(),
-  ]
+    mdx(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
