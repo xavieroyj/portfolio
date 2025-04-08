62fd80316d379936316f4a81b983f51e1ffa849d
Xavier Ong <xavier.oyj1999@gmail.com>
2025-04-08 15:53:38 +0800

    Readded trailingSlash. Using ViewTransition

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xavieroyj.dev',
-
+  trailingSlash: 'ignore',
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
