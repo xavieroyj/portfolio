5e4dcd1e4c397f48fbb095ad890afdfac934058e
Xavier Ong <xavier.oyj1999@gmail.com>
2025-04-08 15:57:26 +0800

    Always use trailingSlash. Make sure header is using trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xavieroyj.dev',
-  trailingSlash: 'ignore',
+  trailingSlash: 'always',
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
